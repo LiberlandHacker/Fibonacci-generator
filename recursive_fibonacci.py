33a23a160a5e908e5d0d394e479af000a60309e9
Jonathan A. McCormick, Jr <67705789+McCormick9@users.noreply.github.com>
2022-02-07 11:59:44 -0500

    Update recursive_fibonacci.py

--- recursive_fibonacci.py
+++ recursive_fibonacci.py
@@ -5,16 +5,16 @@ sequence = [0, 1, 1]  # This is to get the sequence started.
 num -= 3  # This is to correct for the initial sequence specified in the above line. 
 
 def fibonacci(n):
-	n1 = sequence[-2]
-	n2 = sequence[-1]
-	sequence.append(n1+n2)
+    n1 = sequence[-2]
+    n2 = sequence[-1]
+    sequence.append(n1+n2)
 
-	n -= 1
-	if n > 0:
-		fibonacci(n)
+    n -= 1
+    if n > 0:
+        fibonacci(n)
 		
 fibonacci(num)
 
 for number in sequence:
-		print(number)
+    print(number)
 
